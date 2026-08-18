792876c108721bf2b146a8d168ac05f83a37a2a7
fxfranco <shakran91@gmail.com>
2026-08-17 22:30:17 -0500

    Se genera un nuevo endpoint generatenotifications para probar las notificaciones automaticas por signalR

--- Connection360.Api/Controllers/NotificationsController.cs
+++ Connection360.Api/Controllers/NotificationsController.cs
@@ -1,5 +1,7 @@
 ﻿using Connection360.Application.DTOs;
+using Connection360.Application.Enum;
 using Connection360.Application.Ports;
+using Connection360.Application.Ports.Output;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,10 +14,12 @@ namespace Connection360.Api.Controllers
     public class NotificationsController : ControllerBase
     {
         private readonly IGetNotificationsUseCase _getNotificationsUseCase;
+        private readonly INotifierService _notifierService;
 
-        public NotificationsController(IGetNotificationsUseCase getNotificationsUseCase)
+        public NotificationsController(IGetNotificationsUseCase getNotificationsUseCase, INotifierService notifierService)
         {
             _getNotificationsUseCase = getNotificationsUseCase;
+            _notifierService = notifierService;
         }
 
         [HttpGet("allnotifications")]
@@ -27,6 +31,30 @@ namespace Connection360.Api.Controllers
             ClientSummaryRequest request = new ClientSummaryRequest { IdClient = idClient, RoleName = String.Empty };
             List<NotificationsListResponse> result = _getNotificationsUseCase.ExecuteGetNotificationsAllAsync(request, cancellationToken);
             return Ok(result);
+        }
+
+        [HttpGet("generatenotifications")]
+        [AllowAnonymous]
+        //[Authorize(Roles = "ADMIN,CLIENT,ANALISTAOPE,ANALISTASAC")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GenerateNotifications([FromQuery] String idClient, String Message, CancellationToken cancellationToken)
+        {
+            ClientSummaryRequest request = new ClientSummaryRequest { IdClient = idClient, RoleName = String.Empty };
+
+            NotificationsListResponse notificationsListResponse = new NotificationsListResponse
+            {
+                IdNotification = 10,
+                NotificationType = NotificationType.Comment,
+                DocumentNumber = "HBL-5U6HC36K",
+                Title = "Notificacion generada de prueba",
+                Message = Message,
+                MessageDate = DateTime.Now.AddDays(-3),
+                NotificationStatus = NotificationStatus.Unread,
+                NotificationDate = DateTime.Now
+            };
+
+            await _notifierService.SendNotificationToUserAsync(idClient, Message, notificationsListResponse);
+            return Ok("result");
         }
     }
 }
